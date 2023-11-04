6752a7f7168da3f080c79fae233d6d2215a91611
jfarr3UNCC <jfarr3@uncc.edu>
2023-11-04 15:12:30 -0400

    portfolio

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,16 +1,18 @@
 let slideIndex1 = 0;
 let slideIndex2 = 0;
 let slideIndex3 = 0;
+
 showSlides();
 
 function showSlides() {
     let i;
     let e;
-    let u;
+    let o;
 
-    let slides1 = document.getElementsByClassName("proslides");
-    let slides2 = document.getElementsByClassName("perslides");
-    let slides3 = document.getElementsByClassName("clientslides");
+
+    let slides1 = document.getElementsByClassName("slides");
+    let slides2 = document.getElementsByClassName("slideText");
+    let slides3 = document.getElementsByClassName("slideDesc");
 
     for (i = 0; i < slides1.length; i++) {
       slides1[i].style.display = "none";  
@@ -20,18 +22,18 @@ function showSlides() {
     slides1[slideIndex1-1].style.display = "block";  
 
     for (e = 0; e < slides2.length; e++) {
-        slides2[e].style.display = "none";  
-      }
+      slides2[e].style.display = "none";  
+    }
     slideIndex2++;
     if (slideIndex2 > slides2.length) {slideIndex2 = 1}    
     slides2[slideIndex2-1].style.display = "block";  
-
-    for (u = 0; u < slides3.length; u++) {
-      slides3[u].style.display = "none";  
+    
+    for (o = 0; o < slides3.length; o++) {
+      slides3[o].style.display = "none";  
     }
     slideIndex3++;
     if (slideIndex3 > slides3.length) {slideIndex3 = 1}    
     slides3[slideIndex3-1].style.display = "block";  
 
-    setTimeout(showSlides, 4000); // Change image every 2 seconds
+    setTimeout(showSlides, 5000); 
 }
